60a3e3de48b281df1d98a4dce46df1cb740550e9
Makayla <makaylabusher@gmail.com>
2022-12-05 12:22:22 -0600

    add first route

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,2 +1,15 @@
 class PredictionsController < ApplicationController
+  def first
+    fortunes = [
+      "An old friend will come back into your life", 
+      "You will be successful in your career",
+      "You will receive a large amount of money"
+    ]
+    x = rand(0..2)
+  
+    render json: {message: }
+  end
+  #  def second
+  #   render json: {message: }
+  # end
 end
